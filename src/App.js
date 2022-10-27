3b3594133b9a8bf7ab7772dd25e305f6fa152c6d
Andrei Nascimento <andrei.lima.nas@outlook.com>
2022-10-27 13:56:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [contador, setContador] = useState(0)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='contador'>
+      <h1>{contador}</h1>
+      <div className='botoes'>
+        <button onClick={() => setContador(contador + 1)}>+</button>
+        <button onClick={() => setContador(contador - 1)}>-</button>
+      </div>
     </div>
   );
 }
